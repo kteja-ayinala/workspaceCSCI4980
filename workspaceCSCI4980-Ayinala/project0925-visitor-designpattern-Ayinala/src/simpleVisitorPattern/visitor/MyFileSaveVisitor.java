db38ee7416d71e58c585e73a6140af8f9e73a47d
Krishna Teja Ayinala <teja.ayinala@gmail.com>
2018-09-27 13:15:19 -0500

    In sync before class on 0927

--- workspaceCSCI4980-Ayinala/project0925-visitor-designpattern-Ayinala/src/simpleVisitorPattern/visitor/MyFileSaveVisitor.java
+++ workspaceCSCI4980-Ayinala/project0925-visitor-designpattern-Ayinala/src/simpleVisitorPattern/visitor/MyFileSaveVisitor.java
@@ -3,8 +3,6 @@ package simpleVisitorPattern.visitor;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 import simpleVisitorPattern.part.Body;
 import simpleVisitorPattern.part.Brake;
